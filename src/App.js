6c4e48b61cc1594c68adbcf764c2b1baaa283ac3
Debmalya Biswas <viperbale.db@gmail.com>
2023-07-12 11:43:31 +0530

    added config

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './components/Home/Home'
 
 
-axios.defaults.baseURL = 'http://localhost:4000'
+axios.defaults.baseURL = 'https://shunya-eka-hiring-backend.vercel.app/'
 axios.defaults.withCredentials = true
 
 const App = () => {
